25f9b003cac06dbc69bce9d2fa7c0955a51c8f40
Александр Германов <mailmate@yandex.ru>
2023-01-27 01:10:33 +0300

    Feature: добавить интерфейс HistoryManager и класс InMemoryHistoryManager, который его реализует.

---
+++ src/InMemoryHistoryManager.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    //    Новый класс InMemoryHistoryManager должен реализовывать интерфейс HistoryManager.
+    ArrayList<? super Task> historyList;
+
+    public InMemoryHistoryManager() {
+        historyList = new ArrayList<>(10);
+        //должен возвращать последние 10 просмотренных задач;
+    }
+
+    @Override
+    public <T extends Task> void add(T task) {
+        if (historyList.size() == 10){
+            historyList.remove(0);
+        }
+        historyList.add(task);
+    }
+
+    @Override
+    public ArrayList<? super Task> getHistory() {
+        return historyList;
+    }
+}
