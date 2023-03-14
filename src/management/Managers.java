8d1318e9ca67b788e090f6851d03251b3a7f44ce
Александр Германов <mailmate@yandex.ru>
2023-03-14 07:17:34 +0300

    fix: работа по замечаниям.

--- src/management/Managers.java
+++ src/management/Managers.java
@@ -8,6 +8,9 @@ public class Managers {
     public static TaskManager getDefault(){
         return new InMemoryTaskManager();
     }
+    public static TaskManager getFileBacked(){
+        return new FileBackedTaskManager();
+    }
 
     public static HistoryManager getDefaultHistory(){
         return new InMemoryHistoryManager();
